a8a2fa285dca765a1a00fff780944124054215c3
Dipa <tamangdipa77@gmail.com>
2025-09-05 23:11:31 +0545

    Working with multiple packages

---
+++ MainPack/Main.java
@@ -0,0 +1,11 @@
+package MainPack;
+import ShapesPack.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new  Circle();
+        Rectangle r = new Rectangle();
+        
+    }
+
+}
